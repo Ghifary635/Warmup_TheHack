6d3427d5e973ffe3cb370d96b911c9f349549cc2
Ghifary635 <fathurzulakmal@gmail.com>
2025-07-25 08:57:34 +0700

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import pickle
 # 🔧 Download NLTK Resources
 # ================================
 nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
+nltk.download('averaged_perceptron_tagger_eng')
 nltk.download('wordnet')
 nltk.download('omw-1.4')
 
