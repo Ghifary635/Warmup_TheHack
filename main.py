f8392d27efe96de4cb7581198501167b99053441
Ghifary635 <fathurzulakmal@gmail.com>
2025-07-25 08:55:42 +0700

    Update main.py

--- main.py
+++ main.py
@@ -17,9 +17,10 @@ import pickle
 # ================================
 # 🔧 Download NLTK Resources
 # ================================
-nltk.download('stopwords')
-nltk.download('wordnet')
+nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
 
 # ================================
 # ⚙️ Streamlit Config
