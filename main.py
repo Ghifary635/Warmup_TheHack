d6bc3ecfedd6a18fcd2c21b81ea525ccf779e9ca
Ghifary635 <fathurzulakmal@gmail.com>
2025-08-23 05:44:25 +0700

    Update main.py

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger_eng')
 nltk.download('wordnet')
 nltk.download('omw-1.4')
+nltk.download('stopwords')
 
 # ================================
 # ⚙️ Streamlit Config
